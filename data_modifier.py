4d20b9d6951417930ab9eb2a84f46e7122f81d68
lisist <46858327+lisist@users.noreply.github.com>
2019-02-18 14:11:37 +0900

    Create data_modifier.py

---
+++ data_modifier.py
@@ -0,0 +1,47 @@
+## Data modifier
+## Ver 0.1 as of Feb. 2018
+
+import pandas as pd
+
+def yoy(data, freq='m'):
+    ## Calculate Year over Year changes
+    ## Data type is dataframe
+    if freq == 'm' :
+        count = 12
+    elif freq == 'q':
+        count = 4
+    elif freq == 'w':
+        count = 52
+
+    yoy_ch = (data - data.shift(count)) / data.shift(count)
+    yoy_ch.dropna(axis=0, inplace=True)
+
+    return yoy_ch
+
+def qoq(data, freq='m'):
+    ## Calculate Quarter over Quarter
+    ## Data type is dataframe
+    if freq == 'm':
+        count = 4
+    elif freq == 'q':
+        count = 1
+    elif freq == 'w':
+        count = 12
+
+    mom_ch = (data - data.shift(count)) / data.shift(count)
+    mom_ch.dropna(axis=0, inplace=True)
+
+    return mom_ch
+
+def mom(data, freq='m'):
+    ## Calculate Month over Month
+    ## Data type is dataframe
+    if freq == 'm' :
+        count = 1
+    elif freq == 'w':
+        count = 4
+
+    mom_ch = (data - data.shift(count)) / data.shift(count)
+    mom_ch.dropna(axis=0, inplace=True)
+
+    return mom_ch
