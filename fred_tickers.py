a186ae1a6d35532d83938a7b740016f28404c2d7
lisist <46858327+lisist@users.noreply.github.com>
2019-02-20 11:33:24 +0900

    Update fred_tickers.py

--- fred_tickers.py
+++ fred_tickers.py
@@ -4,28 +4,63 @@ import requests
 import bs4 as bs
 import pandas as pd
 
-names = []
-links = []
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime as dt
+from data_modifier import *
 
-for page in range(1,31):
-    resp = requests.get('https://fred.stlouisfed.org/tags/series?t=&et=&pageID='+str(page))
-    soup = bs.BeautifulSoup(resp.text)
-    table = soup.find('table',{'id':'series-pager'})
+def read_tickers(page = 30):
+    names = []
+    links = []
 
-    for rows in table.findAll('tr',{'class':'series-pager-title'})[0:]:
-        link = rows.find('a')
-        link = link.get('href')
-        link = link.strip('/series/')
-        links.append(link)
+    for page in range(1,page+1):
+        resp = requests.get('https://fred.stlouisfed.org/tags/series?t=&et=&pageID='+str(page))
+        soup = bs.BeautifulSoup(resp.text)
+        table = soup.find('table',{'id':'series-pager'})
 
-        if type(rows) is not bs.element.NavigableString:
-            name=rows.text
-            name = name.strip('\n')
-            name = name.strip('\xa0')
-            name = name.strip('\n\n\n\xa0 ')
-            names.append(name)
+        for rows in table.findAll('tr',{'class':'series-pager-title'})[0:]:
+            link = rows.find('a')
+            link = link.get('href')
+            link = link.strip('/series/')
+            links.append(link)
+
+            if type(rows) is not bs.element.NavigableString:
+                name=rows.text
+                name = name.strip('\n')
+                name = name.strip('\xa0')
+                name = name.strip('\n\n\n\xa0 ')
+                names.append(name)
+
+    tickers = pd.DataFrame({'Name':names,'ticker':links})
+    tickers.to_csv('fred_tickers.csv')
+
+def search(quary):
+
+    df = pd.read_csv('fred_tickers.csv',index_col=0)
+    cf = (df['Name'].str.lower()).str.find(quary)
+    name = df['Name'][cf!= -1]
+    ticker = df['ticker'][cf != -1]
+
+    tickers = pd.DataFrame({'Name':name,'ticker':ticker})
+    return tickers
+
+def plot_search_result(start, end, howmany = 10) :
+    for i,j in result[0:howmany].iterrows():
+
+        print(j[0])
+
+        ticker_data = web.DataReader(j[1],'fred',start,end)
+        try:
+            ticker_data.plot(title=j[0])
+        except:
+            pass
+
+    plt.show()
 
 
+result = search('china')
 
-tickers = pd.DataFrame({'Name':names,'ticker':links})
-tickers.to_csv('fred_tickers.csv')
+start = dt.datetime(2010,1,1)
+end = dt.datetime(2019,1,31)
+
+plot_search_result(start,end,3)
