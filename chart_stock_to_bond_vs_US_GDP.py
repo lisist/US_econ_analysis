e70f01c79896a5bed1cdcbfd18c813a8c1d93cf1
lisist <46858327+lisist@users.noreply.github.com>
2019-02-22 22:30:11 +0900

    Update chart_stock_to_bond_vs_US_GDP.py

--- chart_stock_to_bond_vs_US_GDP.py
+++ chart_stock_to_bond_vs_US_GDP.py
@@ -34,11 +34,16 @@ if not os.path.isfile('data/ust10.csv'):
     get_ust10y(start,end)
     print('ust10y file created')
 
+if not os.path.isfile('data/rgdp.csv'):
+    df = web.DataReader('GDPC1', 'fred', start, end)
+    df.to_csv('data/rgdp.csv')
+    print('real gdp file created')
+
 
 ### 기존 CSV 파일에서 Data 받기
 spx = pd.read_csv('data/spx.csv',parse_dates=True, index_col=0)
 ust10 = pd.read_csv('data/ust10.csv',parse_dates=True,index_col=0)
-
+rgdp = pd.read_csv('data/rgdp.csv',parse_dates=True,index_col=0)
 
 # ### 파일 업데이트 여부 체크
 first_date = ust10.index[0]
@@ -53,8 +58,11 @@ if(start < first_date-dt.timedelta(days=1)):
     ust10 = pd.concat([df_ust,ust10])
     df_spx = web.DataReader('^GSPC','yahoo',start,first_date-dt.timedelta(days=1))
     spx = pd.concat([df_spx,spx])
+    df_rgdp = web.DataReader('GDPC1', 'fred', start,first_date-dt.timedelta(days=1))
+    rgdp = pd.concat([df_rgdp, rgdp])
     ust10.to_csv('data/ust10.csv')
     spx.to_csv('data/spx.csv')
+    rgdp.to_csv('data/rgdp.csv')
     print('prior dates updated')
 
 # print(last_date+dt.timedelta(days=1))
@@ -63,28 +71,34 @@ if(end > (last_date+dt.timedelta(days=2))):
     ust10 = pd.concat([ust10,df_ust])
     df_spx = web.DataReader('^GSPC','yahoo',last_date+dt.timedelta(days=1),end)
     spx = pd.concat([spx,df_spx],sort=False)
+    df_rgdp = web.DataReader('GDPC1', 'fred', last_date + dt.timedelta(days=1), end)
+    rgdp = pd.concat([rgdp, df_rgdp], sort=False)
     ust10.to_csv('data/ust10.csv')
     spx.to_csv('data/spx.csv')
+    rgdp = web.DataReader('^GSPC', 'yahoo', last_date + dt.timedelta(days=1), end)
+    spx = pd.concat([spx, df_spx], sort=False)
     print('lates dates updated')
 
 #### Data를 요구 date에 맞게 조작
 
 spx.drop(['High','Low','Volume','Open','Close'],axis=1,inplace=True)
-ust10.index.names=['Date']
+ust10.index.names=['Date']  ## merge를 위해서 ust의 index를 Date로 바꿈
 
 spx = spx[(spx.index >= start) & (spx.index <= end)]
 ust10 = ust10[(ust10.index >= start) & (ust10.index <= end)]
 
 
 
-##############
+##### Data merge and convert it to monthly basis
 
 merged = pd.merge(spx,ust10,how='outer', on='Date')
 merged.dropna(inplace=True)
 
-
 merged  = merged.iloc[merged.reset_index().groupby(merged.index.to_period('M'))['Date'].idxmax()]
 merged = merged.rename(columns={'Adj Close':'Spx','DGS10':'UST10'})
+
+
+###### calculate the return index
 
 spx_index = pd.DataFrame((merged['Spx']/merged['Spx'][0]).values,index = merged.index,columns=['spx index']) ## spx indexation
 ust_index_return = ((merged['UST10']-merged['UST10'].shift(1))*8/100+merged['UST10']/12/100)
@@ -96,10 +110,10 @@ for i in ust_index_return[1:]:
     a = a*(1+i)
     k.append(a)
 
-rgdp = web.DataReader('GDPC1','fred',start,end)
+
+##### get GDP file
+
 rgdp = yoy(rgdp,freq='q')
-
-
 ust_index_return.dropna(inplace= True)
 
 k = pd.DataFrame(k,index=ust_index_return.index,columns=['ust10y'])
